cb3693c6e806fe01cea6f1d21c67ab129d70badd
Eric Vaishnav <ericvaishnav@gmail.com>
2018-11-26 16:49:51 -0800

    wrote DBConnection

---
+++ DBConnection.java
@@ -0,0 +1,65 @@
+import java.sql.*;
+
+public class DBConnection {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+    static final String DB_URL = "jdbc:oracle:thin:@cloud-34-133.eci.ucsb.edu:1521:XE";
+
+    //  Database credentials
+    static final String USERNAME = "hwang00";
+    static final String PASSWORD = "9594458";
+
+	Connection conn = null;
+    Statement stmt = null;
+	
+	public void initialize(){
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            System.out.println("Connected database successfully...");
+			
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+			close();
+        }
+	}
+	
+	public ResultSet sendQuery(String query){
+		try{
+			ResultSet rs = stmt.executeQuery(query);
+			return rs;
+		}catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+			close();
+        }
+	}
+	
+	public void close(){
+		try{
+            conn.close();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    conn.close();
+            }catch(SQLException se){
+            }// do nothing
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+	}
+	
+}//end JDBCExample
